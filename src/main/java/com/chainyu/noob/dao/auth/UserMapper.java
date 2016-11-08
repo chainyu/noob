d9c17ff0887f7cc4366faaf6ca22f0759c6ada53
qianyu.zhang <qianyu.zhang@baozun.com>
2016-11-08 16:11:54 +0800

    test update

--- src/main/java/com/chainyu/noob/dao/auth/UserMapper.java
+++ src/main/java/com/chainyu/noob/dao/auth/UserMapper.java
@@ -7,6 +7,7 @@ import com.chainyu.noob.model.auth.domain.User;
 
 public interface UserMapper extends BaseMapper<User, Long>{
 
+	/*test*/
 	User findByUserName(@Param("userName")String userName);
 
 }
